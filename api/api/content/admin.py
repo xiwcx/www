e6506e2d1bba0b6c1c8cf0e3e6801ff693b100d9
Welch Canavan <welch.canavan@gmail.com>
2018-11-28 20:23:21 -0500

    refine model, admin, and serializer definitions
    closes #6

--- api/api/content/admin.py
+++ api/api/content/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
 
-from .models import Image, Post
+from .models import Image, RelatedImage, Post
 
-
+class ImageInline(admin.StackedInline):
+    model = RelatedImage
+    extra = 1
+    readonly_fields = ('preview',)
 class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('get_preview',)
     fieldsets = [
         (None, {"fields": ["title", "markdown"]}),
-        ("Meta", {"fields": ["hero", "images", "slug", "pub_date", "published"]}),
+        ("Meta", {"fields": ["hero", "get_preview", "slug", "pub_date", "published"]}),
     ]
-    list_display = ("title", "pub_date", "published")
+    list_display = ("title", "get_preview", "pub_date", "published")
+    inlines = [ImageInline]
+
+    def get_preview(self, obj):
+        return obj.hero.preview()
+    get_preview.short_description = 'Hero'
 
 
 admin.site.register(Image)
+admin.site.register(RelatedImage)
 admin.site.register(Post, PostAdmin)
--- api/api/content/models.py
+++ api/api/content/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.utils import timezone
 from .utils import HighlightRenderer
 import mistune
+from django.utils.safestring import mark_safe
 
 
 renderer = HighlightRenderer()
@@ -17,20 +18,23 @@ class Image(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     upload = models.FileField()
 
+    def preview(self):
+        return mark_safe('<img class="image-preview" src="{0}">'.format(self.upload.url))
+
     def __str__(self):
         return self.caption
 
+class RelatedImage(Image):
+    images = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, related_name="images")
 
 # Create your models here.
 class Post(models.Model):
     """Post model"""
 
     markdown = models.TextField()
-    test = models.TextField(blank=True)
     hero = models.OneToOneField(
-        Image, blank=True, on_delete=models.CASCADE, null=True, related_name="hero"
+        Image, blank=True, null=True, on_delete=models.CASCADE, related_name="hero"
     )
-    images = models.ManyToManyField(Image, blank=True, related_name="images")
     pub_date = models.DateTimeField("date published", default=timezone.now)
     published = models.BooleanField(default=False)
     slug = models.SlugField(primary_key=True)
